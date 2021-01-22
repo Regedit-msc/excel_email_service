1c2ee40fc3203701a6cdf60a3e4fdbb46663b528
Regedit-msc <efusanyaae@gmail.com>
2021-01-21 18:10:35 -0800

    second commit

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ app.post('/mail', cors(), (req, res) => {
   `;
     ///-------------------------------
 
-
+    console.log(process.env.USERNAME);
+    console.log(process.env.PASSWORD);
     ///-------------------------------
     var transporter = nodemailer.createTransport({
         service: 'gmail',
